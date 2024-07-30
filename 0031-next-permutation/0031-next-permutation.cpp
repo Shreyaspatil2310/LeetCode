e8900f502cd0b634ed77416459855a25fb80efbe
Shreyas Patil <109668347+Shreyaspatil2310@users.noreply.github.com>
2024-07-30 18:33:57 +0530

    Time: 4 ms (55.83%), Space: 14.8 MB (16.44%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,11 +1,21 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-         if (std::next_permutation(nums.begin(), nums.end())) {
-        std::cout << "Next permutation: ";
-    } else {
-        std::cout << "Array is in the highest permutation, reset to lowest permutation: ";
-    }
-        
+        int len=nums.size();
+        if(len<2){
+            return;
+        }
+        int i=len-2;
+        while(i>=0 && nums[i]>=nums[i+1]){
+            i--;
+        }
+        if(i>=0){
+            int j=len-1;
+            while(j>=0 && nums[j]<=nums[i]){
+                j--;
+            }
+             swap(nums[i],nums[j]);
+        }
+        reverse(nums.begin()+i+1,nums.end());
     }
 };
