a8056c0c381b65d7200482ae7bfeb5b9249155aa
Shreyas Patil <109668347+Shreyaspatil2310@users.noreply.github.com>
2024-08-02 22:20:59 +0530

    Time: 25 ms (24.14%), Space: 27.6 MB (41.24%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
-
-void reverse(vector<int>& nums, int s ,int e){
-    while(s < e){
-        swap(nums[s], nums[e]);
-        s++;
-        e--;
-    }
-}
     void rotate(vector<int>& nums, int k) {
-        k = k % nums.size();
+        int n=nums.size();
+        k %=n;
+        reverse(nums.begin(),nums.end());
 
-        reverse(nums, 0 , nums.size() -1);
-        reverse(nums, 0 , k -1);
-        reverse(nums, k , nums.size() -1);
+        reverse(nums.begin(),nums.begin()+k);
+
+        reverse(nums.begin()+k,nums.end());
     }
 };
