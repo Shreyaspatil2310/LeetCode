c06e357ddc398e5d265dc45fd6b9f4128278fce9
Shreyas Patil <109668347+Shreyaspatil2310@users.noreply.github.com>
2024-08-08 09:32:49 +0530

    Time: 8 ms (66.25%), Space: 12.2 MB (91.44%) - LeetHub

--- 0885-spiral-matrix-iii/0885-spiral-matrix-iii.cpp
+++ 0885-spiral-matrix-iii/0885-spiral-matrix-iii.cpp
@@ -1,18 +1,38 @@
 class Solution {
 public:
-    vector<vector<int>> spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
-        int directions[5] = {0, 1, 0, -1, 0};
-        vector<vector<int>> res = {{rStart, cStart}};
-        int j = 0, n = 0;
-        while (res.size() != rows * cols){
-            for (int i = 0; i < n / 2 + 1; i++){
-                rStart += directions[j], cStart += directions[j+1];
-                if (rStart >= 0 && rStart < rows && cStart >= 0 && cStart < cols)
-                    res.push_back({rStart, cStart});
+    static bool inside(int i, int j, int rows, int cols){
+        return (i>=0)&&(i<rows)&&(j>=0)&&(j<cols);
+    }
+    static vector<vector<int>> spiralMatrixIII(int rows, int cols, int r0, int c0) 
+    {
+        vector<vector<int>> ans(rows*cols);
+        ans[0]={{r0, c0}};
+        int layer=max({rows-r0, cols-c0, r0+1, c0+1})+1;
+        int i=r0, j=c0, k=1;
+        for(int m=1; m<layer; m++){
+            
+            int di=0, dj=1, step=2*m-1;// rightward
+            for(int a=0; a<step; a++){
+                i+=di, j+=dj;
+                if (inside(i,j, rows, cols)) ans[k++]={{i, j}};
             }
-            n++;
-            j = (j + 1) % 4;
+            di=1, dj=0; //downward
+            for(int a=0; a<step; a++){
+                i+=di, j+=dj;
+                if (inside(i,j, rows, cols)) ans[k++]={{i, j}};
+            }
+            di=0, dj=-1;//leftward
+            step++; //increase step
+            for(int a=0; a<step; a++){
+                i+=di, j+=dj;
+                if (inside(i,j, rows, cols)) ans[k++]={{i, j}};
+            }
+            di=-1, dj=0; //upward
+            for(int a=0; a<step; a++){
+                i+=di, j+=dj;
+                if (inside(i,j, rows, cols)) ans[k++]={{i, j}};
+            }
         }
-        return res;        
+        return ans;
     }
 };
