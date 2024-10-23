333de1419e39945ee2d43e1daa3a89add4ff42a4
Shreyas Patil <109668347+Shreyaspatil2310@users.noreply.github.com>
2024-10-23 21:18:09 +0530

    Time: 13 ms (97.55%), Space: 324.2 MB (24.05%) - LeetHub

---
+++ 2641-cousins-in-binary-tree-ii/2641-cousins-in-binary-tree-ii.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    TreeNode* replaceValueInTree(TreeNode* root) {
+        queue<TreeNode*> q;
+        vector<int> levelSum;
+        q.push(root);
+        int n,sum,currSum;
+        
+        while(!q.empty()){
+            n = q.size();
+            sum = 0;
+            while(n--){
+                TreeNode* node = q.front();
+                q.pop();
+                sum += node->val;
+                if(node->left){
+                    q.push(node->left);
+                }
+                if(node->right){
+                    q.push(node->right);
+                }
+            }
+            levelSum.push_back(sum);
+        }
+
+        q.push(root);
+        root->val = 0;
+        int level = 0;
+        while(!q.empty()){
+            n = q.size();
+            sum = level+1 < levelSum.size() ? levelSum[level+1] : 0;
+            while(n--){
+                currSum = 0;
+                TreeNode* node = q.front();
+                q.pop();
+
+                if(node->left){
+                    currSum += node->left->val;
+                    q.push(node->left);
+                }
+                if(node->right){
+                    currSum += node->right->val;
+                    q.push(node->right);
+                }
+                if(node->left)  node->left->val  = sum - currSum;
+                if(node->right) node->right->val = sum - currSum;
+            }
+            level++;
+        }
+
+        return root;
+    }
+};
