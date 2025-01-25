0e2b9bf6d3459af118c2471844a0df78728919ad
Shreyas Patil <109668347+Shreyaspatil2310@users.noreply.github.com>
2025-01-25 13:24:55 +0530

    Time: 0 ms (100%), Space: 10.3 MB (33.94%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void reversemat(vector<vector<int>>& matrix) {
+        for (int i = 0; i < matrix.size(); i++) {
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
+    }
+
+    void transPo(vector<vector<int>>& matrix) {
+        for (int i = 0; i < matrix.size(); i++) {
+            for (int j = i + 1; j < matrix[0].size(); j++) {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+        reversemat(matrix);
+    }
+
+    void rotate(vector<vector<int>>& matrix) {
+        transPo(matrix);
+    }
+};
